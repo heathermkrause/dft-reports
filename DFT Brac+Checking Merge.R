d9162660cea59f8e5d9cb53b47cc3a8edc9b1b46
heathermkrause <heather@idatassist.com>
2015-01-11 11:16:05 -0500

    Initial commit

---
+++ DFT Brac+Checking Merge.R
@@ -0,0 +1,5 @@
+#Merge BRAC DFT data with checkin survey data
+
+data <- read.csv("/Users/heatherkrause/Documents/R/dft-reports/DFTJulyThruNov2104_Analysis.csv")
+Checkin_Data_10_27_2014
+Checkin_Data_11_24_2014
