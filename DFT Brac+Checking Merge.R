f2dc9878dfe1a5d7ab150c4c5385ba39a724ca01
heathermkrause <heather@idatassist.com>
2015-01-11 15:00:36 -0500

    Merginb BRAC data with Checkin data

--- DFT Brac+Checking Merge.R
+++ DFT Brac+Checking Merge.R
@@ -2,6 +2,7 @@
 
 data <- read.csv("/Users/heatherkrause/Documents/R/dft-reports/DFTJulyThruNov2104_Analysis.csv")
 dataO <- dataM[which(dataM$DateF=="2014-10-27"),]
-dataO <- dataM[which(dataM$DateF=="2014-11-24"),]
-Checkin_Data_10_27_2014
-Checkin_Data_11_24_2014
+dataN <- dataM[which(dataM$DateF=="2014-11-24"),]
+
+dataO1 <- read.csv("/Users/heatherkrause/Documents/R/dft-reports/Checkin_Data_10_27_2014.csv")
+dataN1 <- read.csv("/Users/heatherkrause/Documents/R/dft-reports/Checkin_Data_11_24_2014.csv")
