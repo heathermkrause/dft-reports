8db48cd2483c3c59cdd69d7d1d5b2b4b8be8cbdd
heathermkrause <heather@idatassist.com>
2015-01-11 14:51:45 -0500

    catchup commit

--- DFT Brac+Checking Merge.R
+++ DFT Brac+Checking Merge.R
@@ -1,5 +1,7 @@
 #Merge BRAC DFT data with checkin survey data
 
 data <- read.csv("/Users/heatherkrause/Documents/R/dft-reports/DFTJulyThruNov2104_Analysis.csv")
+dataO <- dataM[which(dataM$DateF=="2014-10-27"),]
+dataO <- dataM[which(dataM$DateF=="2014-11-24"),]
 Checkin_Data_10_27_2014
 Checkin_Data_11_24_2014
