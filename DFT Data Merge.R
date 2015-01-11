fb4a645e50c392880521930a2f6030a60824f353
heathermkrause <heather@idatassist.com>
2015-01-11 10:30:46 -0500

    Adding code to do calculations per month

--- DFT Data Merge.R
+++ DFT Data Merge.R
@@ -6,6 +6,54 @@ dataM$month = as.numeric(format(dataM$datetxt, format = "%m"))
 dataM$monthyear <- paste(dataM$month,dataM$year,sep="-")
 dataM$SDVC <- with(dataM,ifelse(is.na(dataM$LG.Code),0,1))
 
+dataM$AveLitre <- capply(dataM$Qty.Ltr.,dataM$DFT.codeCP,mean,na.rm=T)
+dataM$AveFatPerc <- capply(dataM$FatPercent,dataM$DFT.codeCP,mean,na.rm=T)
+dataM$AveSNFPercent <- capply(dataM$SNFPercent,dataM$DFT.codeCP,mean,na.rm=T)
+dataM$AveLitreMonth <- capply(dataM$Qty.Ltr.,paste(dataM$DFT.codeCP,dataM$month),mean,na.rm=T)
+dataM$AveFatPercMonth <- capply(dataM$FatPercent,paste(dataM$DFT.codeCP,dataM$month),mean,na.rm=T)
+dataM$AveSNFPercentMonth <- capply(dataM$SNFPercent,paste(dataM$DFT.codeCP,dataM$month),mean,na.rm=T)
+dataM$AveLitreMonthCP <- capply(dataM$Qty.Ltr.,paste(dataM$Collection.Point.Name,dataM$month),mean,na.rm=T)
+dataM$AveFatPercMonthCP <- capply(dataM$FatPercent,paste(dataM$Collection.Point.Name,dataM$month),mean,na.rm=T)
+dataM$AveSNFPercentMonthCP <- capply(dataM$SNFPercent,paste(dataM$Collection.Point.Name,dataM$month),mean,na.rm=T)
+
+dataM$FatPercJuly <- with(dataM,ifelse(dataM$month=="July",dataM$AveFatPercMonth,0))
+dataM$FatPercAug <- with(dataM,ifelse(dataM$month=="August",dataM$AveFatPercMonth,0))
+dataM$FatPercSept <- with(dataM,ifelse(dataM$month=="September",dataM$AveFatPercMonth,0))
+dataM$FatPercOct <- with(dataM,ifelse(dataM$month=="October",dataM$AveFatPercMonth,0))
+
+dataM$AveLitJuly <- with(dataM,ifelse(dataM$month=="July",dataM$AveLitreMonth,0))
+dataM$AveLitAug <- with(dataM,ifelse(dataM$month=="August",dataM$AveLitreMonth,0))
+dataM$AveLitSept <- with(dataM,ifelse(dataM$month=="September",dataM$AveLitreMonth,0))
+dataM$AveLitOct <- with(dataM,ifelse(dataM$month=="October",dataM$AveLitreMonth,0))
+
+dataM$FatPercJulyCP <- with(dataM,ifelse(dataM$month=="July",dataM$AveFatPercMonthCP,0))
+dataM$FatPercAugCP<- with(dataM,ifelse(dataM$month=="August",dataM$AveFatPercMonthCP,0))
+dataM$FatPercSeptCP <- with(dataM,ifelse(dataM$month=="September",dataM$AveFatPercMonthCP,0))
+dataM$FatPercOctCP <- with(dataM,ifelse(dataM$month=="October",dataM$AveFatPercMonthCP,0))
+
+dataM$AveLitJulyCP <- with(dataM,ifelse(dataM$month=="July",dataM$AveLitreMonthCP,0))
+dataM$AveLitAugCP <- with(dataM,ifelse(dataM$month=="August",dataM$AveLitreMonthCP,0))
+dataM$AveLitSeptCP <- with(dataM,ifelse(dataM$month=="September",dataM$AveLitreMonthCP,0))
+dataM$AveLitOctCP <- with(dataM,ifelse(dataM$month=="October",dataM$AveLitreMonthCP,0))
+
+dataM$FatDiffJulyAug <- dataM$FatPercAug - dataM$FatPercJuly #large numbers is big increase
+dataM$FatDiffAugSept <- dataM$FatPercSept - dataM$FatPercAug #large numbers is big increase
+dataM$FatDiffSeptOct <- dataM$FatPercOct - dataM$FatPercSept #large numbers is big increase
+
+dataM$LitreDiffJulyAug <- dataM$AveLitAug - dataM$AveLitJuly  #large numbers is big increase
+dataM$LitreDiffAugSept <- dataM$AveLitSept - dataM$AveLitAug #large numbers is big increase
+dataM$LitreDiffSeptOct <- dataM$AveLitOct - dataM$AveLitSept #large numbers is big increase
+
+dataM$FatDiffJulyAugCP <- dataM$FatPercAugCP - dataM$FatPercJulyCP #large numbers is big increase
+dataM$FatDiffAugSeptCP <- dataM$FatPercSeptCP - dataM$FatPercAugCP #large numbers is big increase
+dataM$FatDiffSeptOctCP <- dataM$FatPercOctCP - dataM$FatPercSeptCP #large numbers is big increase
+
+dataM$LitreDiffJulyAugCP <- dataM$AveLitAugCP - dataM$AveLitJulyCP  #large numbers is big increase
+dataM$LitreDiffAugSeptCP <- dataM$AveLitSeptCP - dataM$AveLitAugCP #large numbers is big increase
+dataM$LitreDiffSeptOctCP <- dataM$AveLitOctCP - dataM$AveLitSeptCP #large numbers is big increase
+
+write.csv(dataM,"/Users/heatherkrause/Desktop/DFTJulyThruOct2104.csv")
+
 
 
 
