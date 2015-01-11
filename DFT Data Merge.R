3b7bf6b7c20e5adc3eb7e41f8fd0f6b71ef9d1af
heathermkrause <heather@idatassist.com>
2015-01-11 11:01:44 -0500

    Final derived variables and merge

--- DFT Data Merge.R
+++ DFT Data Merge.R
@@ -48,7 +48,8 @@ data1234MF <- data1234M[which(data1234M$year < 2015),]
                      
 write.csv(data1234MF,"/Users/heatherkrause/Documents/R/dft-reports/DFTJulyThruNovember2104_Analysis.csv")
 
-dataM <- read.csv("/Users/heatherkrause/Desktop/DFTJulyThruOct2104.csv")
+dataM <- data1234MF
+
 dataM$AveLitre <- capply(dataM$Qty.Ltr.,dataM$DFT.codeCP,mean,na.rm=T)
 dataM$AveFatPerc <- capply(dataM$FatPercent,dataM$DFT.codeCP,mean,na.rm=T)
 dataM$AveSNFPercent <- capply(dataM$SNFPercent,dataM$DFT.codeCP,mean,na.rm=T)
@@ -70,31 +71,42 @@ dataM$AveLitJuly <- with(dataM,ifelse(dataM$month=="July",dataM$AveLitreMonth,0)
 dataM$AveLitAug <- with(dataM,ifelse(dataM$month=="August",dataM$AveLitreMonth,0))
 dataM$AveLitSept <- with(dataM,ifelse(dataM$month=="September",dataM$AveLitreMonth,0))
 dataM$AveLitOct <- with(dataM,ifelse(dataM$month=="October",dataM$AveLitreMonth,0))
+dataM$AveLitNov <- with(dataM,ifelse(dataM$month=="November",dataM$AveLitreMonth,0))
+dataM$AveLitDec <- with(dataM,ifelse(dataM$month=="December",dataM$AveLitreMonth,0))
 
 dataM$FatPercJulyCP <- with(dataM,ifelse(dataM$month=="July",dataM$AveFatPercMonthCP,0))
 dataM$FatPercAugCP<- with(dataM,ifelse(dataM$month=="August",dataM$AveFatPercMonthCP,0))
 dataM$FatPercSeptCP <- with(dataM,ifelse(dataM$month=="September",dataM$AveFatPercMonthCP,0))
 dataM$FatPercOctCP <- with(dataM,ifelse(dataM$month=="October",dataM$AveFatPercMonthCP,0))
+dataM$FatPercNovCP <- with(dataM,ifelse(dataM$month=="November",dataM$AveFatPercMonthCP,0))
+dataM$FatPercDecCP <- with(dataM,ifelse(dataM$month=="December",dataM$AveFatPercMonthCP,0))
 
 dataM$AveLitJulyCP <- with(dataM,ifelse(dataM$month=="July",dataM$AveLitreMonthCP,0))
 dataM$AveLitAugCP <- with(dataM,ifelse(dataM$month=="August",dataM$AveLitreMonthCP,0))
 dataM$AveLitSeptCP <- with(dataM,ifelse(dataM$month=="September",dataM$AveLitreMonthCP,0))
 dataM$AveLitOctCP <- with(dataM,ifelse(dataM$month=="October",dataM$AveLitreMonthCP,0))
+dataM$AveLitNovCP <- with(dataM,ifelse(dataM$month=="November",dataM$AveLitreMonthCP,0))
+dataM$AveLitDecCP <- with(dataM,ifelse(dataM$month=="December",dataM$AveLitreMonthCP,0))
 
 dataM$FatDiffJulyAug <- dataM$FatPercAug - dataM$FatPercJuly #large numbers is big increase
 dataM$FatDiffAugSept <- dataM$FatPercSept - dataM$FatPercAug #large numbers is big increase
 dataM$FatDiffSeptOct <- dataM$FatPercOct - dataM$FatPercSept #large numbers is big increase
+dataM$FatDiffOctNov <- dataM$FatPercNov - dataM$FatPercOct #large numbers is big increase
 
 dataM$LitreDiffJulyAug <- dataM$AveLitAug - dataM$AveLitJuly  #large numbers is big increase
 dataM$LitreDiffAugSept <- dataM$AveLitSept - dataM$AveLitAug #large numbers is big increase
 dataM$LitreDiffSeptOct <- dataM$AveLitOct - dataM$AveLitSept #large numbers is big increase
+dataM$LitreDiffOctNov <- dataM$AveLitNov - dataM$AveLitOct #large numbers is big increase
 
 dataM$FatDiffJulyAugCP <- dataM$FatPercAugCP - dataM$FatPercJulyCP #large numbers is big increase
 dataM$FatDiffAugSeptCP <- dataM$FatPercSeptCP - dataM$FatPercAugCP #large numbers is big increase
 dataM$FatDiffSeptOctCP <- dataM$FatPercOctCP - dataM$FatPercSeptCP #large numbers is big increase
+dataM$FatDiffOctNovCP <- dataM$FatPercNovCP - dataM$FatPercOctCP #large numbers is big increase
 
 dataM$LitreDiffJulyAugCP <- dataM$AveLitAugCP - dataM$AveLitJulyCP  #large numbers is big increase
 dataM$LitreDiffAugSeptCP <- dataM$AveLitSeptCP - dataM$AveLitAugCP #large numbers is big increase
 dataM$LitreDiffSeptOctCP <- dataM$AveLitOctCP - dataM$AveLitSeptCP #large numbers is big increase
+dataM$LitreDiffOctNovCP <- dataM$AveLitNovCP - dataM$AveLitOctCP #large numbers is big increase
 
-write.csv(dataM,"/Users/heatherkrause/Desktop/DFTJulyThruOct2104.csv")
+write.csv(dataM,"/Users/heatherkrause/Documents/R/dft-reports/DFTJulyThruNov2104_Analysis.csv")
+write.csv(dataM,"/Users/heatherkrause/Documents/R/dft-reports/DFTJulyThruNov2104_Tableau.csv")
