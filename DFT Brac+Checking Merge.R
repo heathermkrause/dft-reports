debff1216613124ba0bbe872e47e3d7135d0bf2c
heathermkrause <heather@idatassist.com>
2015-01-11 15:06:39 -0500

    Getting data merged

--- DFT Brac+Checking Merge.R
+++ DFT Brac+Checking Merge.R
@@ -1,8 +1,8 @@
 #Merge BRAC DFT data with checkin survey data
 
-data <- read.csv("/Users/heatherkrause/Documents/R/dft-reports/DFTJulyThruNov2104_Analysis.csv")
-dataO <- dataM[which(dataM$DateF=="2014-10-27"),]
-dataN <- dataM[which(dataM$DateF=="2014-11-24"),]
+data <- read.csv("/Users/heatherkrause/Documents/R/dft-reports/DFTJulyThruNovember2104_Analysis.csv")
+dataO <- data[which(data$DateF=="2014-10-27"),]
+dataN <- data[which(data$DateF=="2014-11-24"),]
 
 dataO1 <- read.csv("/Users/heatherkrause/Documents/R/dft-reports/Checkin_Data_10_27_2014.csv")
 dataN1 <- read.csv("/Users/heatherkrause/Documents/R/dft-reports/Checkin_Data_11_24_2014.csv")
